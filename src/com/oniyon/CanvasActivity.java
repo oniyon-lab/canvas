dbe523aac560c5fe544f2531b824a87477f6db6c
junnm <test@oniyon.sakura.ne.jp>
2012-08-23 17:35:16 +0900

    2nd

--- src/com/oniyon/CanvasActivity.java
+++ src/com/oniyon/CanvasActivity.java
@@ -8,6 +8,7 @@ public class CanvasActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        setContentView(new MainView(this));
     }
 }
